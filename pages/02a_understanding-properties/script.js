7c6172fd1c5ae08aeb52a471569b7b43df732787
PieMenard <pinedo.m.gustavo@gmail.com>
2026-03-24 16:08:57 -0400

    complete 02a

--- pages/02a_understanding-properties/script.js
+++ pages/02a_understanding-properties/script.js
@@ -1,1 +1,13 @@
+import gsap from 'gsap';
 
+gsap.to('.box', {
+    y: -50,
+    opacity: 1,
+    rotation: 360,
+    background: '#ff6f61',
+    borderRadius: '50%',
+    scale: 1.25,
+    duration: 1,
+    delay: 0.5,
+    ease: 'power1.inOut',
+});
